0952e59771c940db9b27e5ecb2646810c944e35f
WIN-LIN <puppy95176@gmail.com>
2024-08-22 16:20:27 +0800

    change default PORT

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
